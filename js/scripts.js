2cf6e102eea807a8df0f89620ff3d7d1c9851daa
Rufei <rufeisheng@gmail.com>
2019-04-11 18:38:51 -0400

    update

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,11 @@ $('#about-us').on('click', function() {
   $('.introduction').css("background-color", "#F1E8F7");
 });
 
-// $('#emailSubmit').on('click', function(){
-//   // var email = $('#emailInput').val();
-//   $('.outputMessage').html(`Thank you for your interest! We will contact you soon.`);
-// })
+$('#Submit').on('click', function() {
+	alert('Thank you for join us!');
+});
+
+
 $(document).ready(function() {
 $('.popup-with-zoom-anim').magnificPopup({
   type: 'inline',
@@ -22,69 +23,3 @@ $('.popup-with-zoom-anim').magnificPopup({
   removalDelay: 300,
   mainClass: 'my-mfp-zoom-in'
 });
-
-
-
-
-// $('#form-group').on('click', function()
-// {
-//     function after_form_submitted(data)
-//     {
-//         if(data.result == 'success')
-//         {
-//             $('form#reused_form').hide();
-//             $('#success_message').show();
-//             $('#error_message').hide();
-//         }
-//         else
-//         {
-//             $('#error_message').append('<ul></ul>');
-//
-//             jQuery.each(data.errors,function(key,val)
-//             {
-//                 $('#error_message ul').append('<li>'+key+':'+val+'</li>');
-//             });
-//             $('#success_message').hide();
-//             $('#error_message').show();
-//
-//             //reverse the response on the button
-//             $('button[type="button"]', $form).each(function()
-//             {
-//                 $btn = $(this);
-//                 label = $btn.prop('orig_label');
-//                 if(label)
-//                 {
-//                     $btn.prop('type','submit' );
-//                     $btn.text(label);
-//                     $btn.prop('orig_label','');
-//                 }
-//             });
-//
-//         }//else
-//     }
-//
-// 	$('#reused_form').submit(function(e)
-//       {
-//         e.preventDefault();
-//
-//         $form = $(this);
-//         //show some response on the button
-//         $('button[type="submit"]', $form).each(function()
-//         {
-//             $btn = $(this);
-//             $btn.prop('type','button' );
-//             $btn.prop('orig_label',$btn.text());
-//             $btn.text('Sending ...');
-//         });
-//
-//
-//                     $.ajax({
-//                 type: "POST",
-//                 url: 'handler.php',
-//                 data: $form.serialize(),
-//                 success: after_form_submitted,
-//                 dataType: 'json'
-//             });
-//
-//       });
-// });
